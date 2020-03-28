7b2994dd19477640b40bf71d50cb2bfc6aa59933
daxing.li <daxing_li@cathaypacific.com>
2020-03-28 11:50:11 +0800

    daxing.li step1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 function Numbers(props) {
   return (
     <div className="number_warp">
-      numbers{/* 根据props传入的数据生成按钮 */}
+     {props.nums.map(e =>
+        <button key={e.label}>
+          {e.label}
+        </button>
+     )}
     </div>
   );
 }
@@ -44,7 +48,11 @@ const buttons = [
 function Actions(props) {
   return (
     <div className="action_warp">
-      actions {/* 根据props传入的数据生成按钮 */}
+      {props.funs.map(e =>
+          <button key={e.label}>
+            {e.label}
+          </button>
+      )}
     </div>
   );
 }
@@ -72,38 +80,16 @@ const funs = [
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.errorMsg = props.errorMsg || "请输入正确的算术式!";
-    this.state = {
-      evalStr: "",
-      result: ""
-    };
+
   }
-  //算式方法组件点击后处理方法
-  actionsClick = e => {
-    // 如果有错误信息，只能点击C按钮
-    // 如果点击C按钮清除 错误信息,需要计算的算式，计算结果
-    // 如果点击的=,计算算式。 如果错误显示错误提示
-    // 如果点击是一般计算方法符号，在当前算术式后面累加
-  };
-  //数字按钮点击后的处理方法
-  numClick = e => {
-    //直接在当前算式后面累加输入的数字
-  };
-  //手动修改算式的处理方法
-  inputOnChange = e => {
-    e.persist();
-    //算式显示区域可以手动修改算式
-  };
+
   render() {
     return (
       <div className="warp">
         <input />
         <div className="result" />
-        {/* 显示计算结果和错误提示 */}
-
-        {/* 引入方法按钮组件 */}
-
-        {/* 引入数字按钮组件 */}
+        <Actions funs={funs}/>
+        <Numbers nums={buttons}/>
       </div>
     );
   }
