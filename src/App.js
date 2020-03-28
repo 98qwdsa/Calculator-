9b7eaacf001138e5e153de10f735b0320f7acc1a
daxing.li <daxing_li@cathaypacific.com>
2020-03-28 13:19:59 +0800

    daxing.li step5

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React from "react";
 import "./App.css";
+function PWDstrength(str) {
+    if (str.length > 8) {
+        return "Strong";
+    }
+    if (str.length > 4) {
+        return "medium";
+    }
+    if (str.length <= 4) {
+        return "weak";
+    }
+}
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  onSubmit(e) {
-    e.persist();
-    console.log(e.target.name.value,e.target.pwd.value);
-    e.preventDefault();
-  }
-  render() {
-    return (
-        <form className="warp" onSubmit={this.onSubmit}>
-          <label>
-            name:<input name="name" />
-          </label>
-          <label>
-            pwd:<input name="pwd" />
-          </label>
-          <button type="submit" value="Submit">
-            submit
-          </button>
-        </form>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            pwd: ""
+        };
+    }
+    onSubmit(e) {
+        e.persist();
+        console.log(this.state);
+    }
+    onChange = e => {
+        e.persist();
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+    render() {
+        return (
+            <div className="warp">
+                <label>
+                    name:<input
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                />
+                </label>
+                <label>
+                    pwd:<input
+                    name="pwd"
+                    value={this.state.pwd}
+                    onChange={this.onChange}
+                />
+                </label>
+                <p>
+                    {this.state.pwd && `密码强度:${PWDstrength(this.state.pwd)}`}
+                </p>
+                <button onClick={this.onSubmit}>submit</button>
+            </div>
+        );
+    }
 }
 
 export default App;
