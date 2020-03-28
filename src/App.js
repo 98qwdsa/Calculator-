e51f96bb3b311410f33bfbfc753aceee7dc1fbd1
daxing.li <daxing_li@cathaypacific.com>
2020-03-28 13:14:06 +0800

    daxing.li step4

--- src/App.js
+++ src/App.js
@@ -1,161 +1,27 @@
 import React from "react";
 import "./App.css";
-class Numbers extends React.Component {
-  render(){
-    return (
-      <>
-      <div className="number_warp">
-        {this.props.nums.map(e =>
-          <button
-            key={e.label}
-            onClick={() => {
-              this.props.click(e.label);
-            }}
-          >
-            {e.label}
-          </button>
-        )}
-      </div>
-      <div className="result">
-        {this.props.str}
-      </div>
-      </>
-    );
-  }
-}
-const buttons = [
-  {
-    label: "0"
-  },
-  {
-    label: "1"
-  },
-  {
-    label: "2"
-  },
-  {
-    label: "3"
-  },
-  {
-    label: "4"
-  },
-  {
-    label: "5"
-  },
-  {
-    label: "6"
-  },
-  {
-    label: "7"
-  },
-  {
-    label: "8"
-  },
-  {
-    label: "9"
-  }
-];
-class Actions extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      str:this.props.str
-    }
-  }
-  componentWillReceiveProps(nextProps){
-    // this.setState({
-    //   str:`+${nextProps.str}+`
-    // })
-  }
-  render(){
-    return (
-      <>
-      <div className="action_warp">
-        {this.props.funs.map(e =>
-          <button
-            key={e.label}
-            onClick={() => {
-              this.props.click(e.label);
-              this.setState({
-                str:`+${e.label}+`
-              })
-            }}
-          >
-            {e.label}
-          </button>
-        )}
-      </div>
-      <div className="result">
-        {this.props.str}
-      </div>
-      <div className="result">
-        this is init by this.props.str {this.state.str}
-      </div>
-      </>
-    );
-  }
-}
-const funs = [
-  {
-    label: "+"
-  },
-  {
-    label: "-"
-  },
-  {
-    label: "*"
-  },
-  {
-    label: "/"
-  },
-  {
-    label: "C"
-  },
-  {
-    label: "="
-  }
-];
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      str: ""
-    };
   }
-  actionsClick = e => {
-    this.setState({
-      str: e
-    });
-  };
-  numClick = e => {
-    this.setState({
-      str: e
-    });
-  };
-  inputOnChange = e => {
+  onSubmit(e) {
     e.persist();
-    this.setState({
-      str: e.target.value
-    });
-  };
+    console.log(e.target.name.value,e.target.pwd.value);
+    e.preventDefault();
+  }
   render() {
     return (
-      <div className="warp">
-        <input value={this.state.str} onChange={this.inputOnChange} />
-        <div className="result">
-          {this.state.str}
-        </div>
-        <Actions
-          str={this.state.str}
-          funs={funs}
-          click={this.actionsClick}
-        />
-        <Numbers
-          str={this.state.str}
-          nums={buttons}
-          click={this.numClick}
-        />
-      </div>
+        <form className="warp" onSubmit={this.onSubmit}>
+          <label>
+            name:<input name="name" />
+          </label>
+          <label>
+            pwd:<input name="pwd" />
+          </label>
+          <button type="submit" value="Submit">
+            submit
+          </button>
+        </form>
     );
   }
 }
