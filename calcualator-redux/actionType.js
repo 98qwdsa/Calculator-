51e73185dc9d554fc641cda7ded1b5c9a77d9ffc
chen261155 <1554758745@qq.com>
2020-09-17 10:43:04 +0800

    botong.chen_calculator_redux

---
+++ calcualator-redux/actionType.js
@@ -0,0 +1,5 @@
+export const CHANGE_INPUT_VALUE = 'change_input_value';
+export const CLICK_C_BUTTON = 'click_c_button';
+export const CLIAC_EVAL = 'click_eval';
+export const CHANGE_INPUT_E ='change_input_e';
+export const CLICK_NUMBUTTON_ACTION = 'click_numbutton_action';
